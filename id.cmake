0314f2840f935f2fc69d39e823cc736bf43302c0
David Callu <callu.david@gmail.com>
2018-10-16 09:12:16 +0200

    chore(version): bump to 2.6.1

--- id.cmake
+++ id.cmake
@@ -1,14 +1,14 @@
 set(package freetype)
-set(version 2.5.5)
+set(version 2.6.1)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-3
+    VERSION ${version}-1
     ABI 6
     )
 
 byd__package__download_info(${package}
-    URL "https://download.savannah.gnu.org/releases/freetype/freetype-2.5.5.tar.gz"
-    URL_HASH SHA1=884830e13a4ebd780150697bab7e172e902194c9
+    URL "https://download.savannah.gnu.org/releases/freetype/freetype-2.6.1.tar.gz"
+    URL_HASH SHA1=1684764491618187e5dd49f50e64c08dbdb861c8
 )
